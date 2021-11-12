76d1efbb1315e601a588c145c57db38d4332c65a
Joshua Minnie <josh.minnie@gmail.com>
2021-11-12 11:11:03 -0500

    Add file generation for service containers (#2)
    * Add file generation for service containers
    
    * Change platform to be optional

--- lib/workspace/commands/service.rb
+++ lib/workspace/commands/service.rb
@@ -4,11 +4,39 @@ module Workspace
       include Thor::Actions
       include Workspace::Docker
 
+      # Gives the Thor commands a root directory to operate from.
+      def self.source_root
+        File.dirname(File.expand_path('../../../workspace', __dir__))
+      end
+
+      desc 'new SERVICE', 'Create a new service from template'
+      option :image, type: :string, required: true, aliases: :i, desc: 'base image for the container'
+      option :platform, type: :string, aliases: :p, desc: 'platform for the container'
+      option :ignore, type: :boolean, desc: 'ignore this new service; do not include it as tracked files'
+      def new(service)
+        root = service_root(service)
+
+        empty_directory("#{root}")
+        empty_directory("#{root}/data")
+        create_file("#{root}/data/.gitkeep")
+        template('lib/generators/services/Dockerfile', "#{root}/Dockerfile", image: options[:image])
+        props = { service: service }
+        props.merge!(platform: options[:platform]) if options[:platform]
+        template('lib/generators/services/docker-compose.yml', "#{root}/docker-compose.yml", **props)
+        copy_file('lib/generators/services/.dockerignore', "#{root}/.dockerignore")
+        insert_into_file('workspace.json', "\n    \"#{service}\": { \"environments\": [] },", after: '"services": {')
+
+        unless options[:ignore]
+          insert_into_file('.gitignore', "\n!#{root}", after: '# BEGIN allowlisting services')
+          insert_into_file('workspace.sample.json', "\n    \"#{service}\": { \"environments\": [] },", after: '"services": {')
+        end
+      end
+
       desc 'start SERVICE', 'Starts up the specified service'
       option :build, type: :boolean
       def start(service)
         root = service_root(service)
-        environments = Workspace.configuration.services.dig(service, 'environments')
+        environments = Workspace.configuration.services.dig(service, 'environments') || []
 
         create_file("#{root}/.env")
         environments.each do |env|
