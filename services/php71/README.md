325c90d4553f81693fbc226e750bc8bd3cd65336
Josh Pennington <joshpennington@users.noreply.github.com>
2021-11-29 11:55:56 -0500

    Add Standalone PHP-FPM (#38)
    * Adds PHP 8.0
    
    * Add PHP 5.4
    
    * Add PHP 5.5
    
    * Add PHP 5.6
    
    * Add PHP 7.0
    
    * Add PHP 7.1
    
    * Add PHP 7.2
    
    * Add PHP 7.3
    
    * Add PHP 7.4
    
    * Add remaining files

---
+++ services/php71/README.md
@@ -0,0 +1,9 @@
+# PHP-FPM 7.1
+
+# Setup
+
+There is a script called `build.sample.sh` that can be copied to `build.sh`. When `build.sh` exists this script will be executed
+when the container for that PHP version is built.
+
+Note: Properly configuring PHP containers is outside the scope of this document. Please see https://hub.docker.com/_/php for how to
+properly configure a PHP Docker container.
---
+++ services/php80/build.sample.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+# This script is for installing additional PHP extensions for the PHP 8.0 container. See https://hub.docker.com/_/php for more
+# details (Search on page for "How to install more PHP extensions"). I have added a the commands to install both Composer 1 and 2
+# for your convenience.
+
+# Install Composer 1 & 2
+wget https://getcomposer.org/download/latest-2.x/composer.phar
+chmod +x composer.phar
+mv composer.phar /usr/local/bin/composer
+
+wget https://getcomposer.org/download/latest-1.x/composer.phar
+chmod +x composer.phar
+mv composer.phar /usr/local/bin/composer1
